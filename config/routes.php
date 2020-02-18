d6af8b9e623e2172fdfab5dd76c9c95953cc3714
Fzammit <frederic.zammit@hotmail.fr>
2020-02-18 11:02:17 +0100

    room controller updated

--- config/routes.php
+++ config/routes.php
@@ -11,31 +11,30 @@ $router->get('', 'PagesController@index'); // Page d'accueil contenant entre aut
 /**
  * Routes ROOM
  */
-$router->get('/rooms/', 'RoomsController@index');
+$router->get('/rooms/', 'RoomsController@index');// Afficher l'ensemble des rooms
 $router->get('/rooms/(\d+)', 'RoomsController@show'); // Affichage de 1 room
 
-$router->get('/rooms/new', 'RoomsController@new');
-$router->post('/rooms/new', 'RoomsController@create');
+$router->get('/rooms/new', 'RoomsController@new');// Affichage du formulaire
+$router->post('/rooms/new', 'RoomsController@create');// Création d'une room
 
-$router->get('/rooms/(\d+)/edit', 'RoomsController@edit');
-$router->post('/rooms/(\d+)/edit', 'RoomsController@update');
+$router->get('/rooms/(\d+)/edit', 'RoomsController@edit');// Edition d'une room
+$router->post('/rooms/(\d+)/edit', 'RoomsController@update');// mise à jour d'une room
 
-$router->get('/rooms/(\d+)/delete/', 'RoomsController@delete');
+$router->get('/rooms/(\d+)/delete/', 'RoomsController@delete');// Supprimer une room
 
 
 /**
  * Routes CLIENT
  */
-$router->get('/clients', 'ClientsController@index');
+$router->get('/clients', 'ClientsController@index');// Affichage de l'ensemble des Clients
 $router->get('/clients/(\d+)', 'ClientsController@show');// Affichage de 1 client
 
-$router->get('/clients/new', 'ClientsController@new');
-$router->post('/clients/new', 'ClientsController@create');
+$router->get('/clients/new', 'ClientsController@new');// Afichage du formulaire
+$router->post('/clients/new', 'ClientsController@create');// Création d'un client
 
-$router->get('/clients/(\d+)/edit', 'ClientsController@edit');
-$router->post('/clients/(\d+)/edit', 'ClientsController@update');
+$router->get('/clients/(\d+)/edit', 'ClientsController@edit');// Edition d'un client
+$router->post('/clients/(\d+)/edit', 'ClientsController@update');// Mise à jour d'un client
 
-$router->get('/clients/(\d+)/delete', 'ClientsController@delete');
-
+$router->get('/clients/(\d+)/delete', 'ClientsController@delete');// Suppression 'un client
 
 $router->run();
--- src/controller/RoomsController.php
+++ src/controller/RoomsController.php
@@ -17,4 +17,58 @@ class RoomsController extends AbstractController {
             'room' => $room
         ]);
     }
+
+    public function index() {
+        //récupérer les rooms
+        $room = $this->container->getRoomManager()->findAll();
+        //Afficher les rooms
+        echo $this->container->getTwig()->render('rooms/index.html.twig', [
+            'room' => $room
+        ]);
+    }
+
+    /**
+     * Affichage du formulaire de création
+     * GET /room/new
+     */
+    public function new() {
+        echo $this->container->getTwig()->render('rooms/new.html.twig');
+    }
+
+    /**
+     * Traitement du formulaire de création puis redirection vers la liste des rooms
+     * POST / rooms/new
+     */
+    public function create() {
+        $this->container->getRoomManager()->create($_POST);
+        $this->index();
+    }
+
+    /**
+     * Affichage du formulaire d'édition
+     * GET /rooms/new
+     */
+    public function edit(int $id) {
+        $room = $this->container->getRoomManager()->findOneById($id);
+
+        echo $this->container->getTwig()->render('rooms/new.html.twig', ['room' =>$room]);
+    }
+
+    /**
+     * Traitement du formulaire d'édition puis redirection vrs l'index des rooms
+     * POST /rooms/new 
+     */
+    public function update(int $id) {
+        $this->container->getRoomManager()->update($id, $_POST);
+        $this->show($id);
+    }
+
+    /**
+     * suppression d'une room
+     * GET /rooms/:id/delete
+     */
+    public function delete(int $id) {
+        $this->container->getRoomManager()->delete($id);
+        $this->index();
+    }
 }
