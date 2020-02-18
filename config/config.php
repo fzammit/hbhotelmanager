7bf88077202e4ade30f42f12c00f8928ad3d3e36
Fzammit <frederic.zammit@hotmail.fr>
2020-02-18 09:26:36 +0100

    Message du commit

---
+++ config/config.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Service\ServiceContainer;
+
+$configuration = [
+    'db' => [
+        'dsn'      => 'mysql:dbname=hbhotelmanager;host=localhost;port=3306;charset=utf8',
+        'username' => 'root',
+        'password' => '',
+    ],
+    'env' => [
+        'base_path' => 'http://localhost/mvc-exam',
+        'site_name' => 'HB Hotel Manager'
+    ]
+];
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$container = new ServiceContainer($configuration);
+
+require_once __DIR__ . '/routes.php';
